e14444b7f4e2a0df1de33b5c77982e7b56691e34
Anna Sclarandis <116303239+ASclarandis@users.noreply.github.com>
2022-11-17 15:50:33 -0800

    Delete Basic JavaScript Projects/Project2_functions directory

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++
@@ -1,6 +0,0 @@
-function myFunction() { //Defining and naming a function
-    var sentence ="My favorite color"; //Assigning a string value to a variable
-    sentence += " is pink!"; //Using += to concatenate string values
-    document.getElementById("Concatenate").innerHTML= sentence; //Document will display different text
-    document.getElementById("Concatenate").style.color="pink"; //Document will display different color
-}
